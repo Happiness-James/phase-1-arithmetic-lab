dd09a650fb89253140269aba50a191ee20fcda85
Happiness-James <gesarehjames@gmail.com>
2022-11-22 20:30:28 +0000

    commit tree 512b30757f061f04d0d606b9192f9e7ceac8b8c4

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+const random= (Math.ceil(Math.random()) + 0)
+let num3= 24;
+let num4=10;
+const mod = num3 % num4;
+console.log(mod);
+const max =Math.max(4,6,8,9,5,2,1,13,18,20,12,11)
+console.log(max)
